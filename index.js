6b0468f54a649ab3afc0e32cc6a746476e20af20
ArthurG1000 <garnierarthur.contact@gmail.com>
2021-09-29 16:05:56 +0200

    added navbar

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ contactForm.addEventListener("submit", function (event) {
   alert("Votre message a été envoyé");
 });
 */
+function showResponsiveMenu() {
+  let menu = document.getElementById("topnav_responsive_menu");
+  let icon = document.getElementById("topnav_hamburger_icon");
+  let root = document.getElementById("root");
+  if (menu.className === "") {
+      menu.className = "open";
+      icon.className = "open";
+      root.style.overflowY = "hidden";
+  } else {
+      menu.className = "";                    
+      icon.className = "";
+      root.style.overflowY = "";
+  }
+}
 
 var contactForm = document.getElementById("contactForm")
 contactForm.addEventListener("submit", function (event) {
