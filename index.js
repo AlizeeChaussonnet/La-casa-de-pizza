454dba4d3059fa9afb4b3116179340933dc68444
Alizée <alizee.chaussonnet@gmail.com>
2021-09-29 14:11:44 +0200

    Ajout du formulaire

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+/*var contactForm = document.getElementById("contactForm")
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Votre message a été envoyé");
+});
+*/
+
 var contactForm = document.getElementById("contactForm")
 contactForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  alert(message);
+  contactForm.style.backgroundColor = "white";
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  contactForm.innerHTML = `Merci pour votre message ${firstName} ${lastName}.`;
+  contactForm.style.fontFamily = "Lato, sans-serif";
+  contactForm.style.justifyContent = "center";
 });
+
