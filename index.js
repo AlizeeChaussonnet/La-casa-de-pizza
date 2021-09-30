1ffed741ba99965b59d8c8684978b41999c8a29b
Alizée <alizee.chaussonnet@gmail.com>
2021-09-30 22:23:35 +0200

    Ajout du module roulette, manque le stop dessus

--- index.js
+++ index.js
@@ -31,3 +31,79 @@ contactForm.addEventListener("submit", function (event) {
   contactForm.style.justifyContent = "center";
 });
 
+// Déclaration des constantes
+const professeur = document.querySelector("#image-selection");
+const button = document.querySelector("#button-professeur")
+const prenom = document.querySelector("#prenom");
+
+const arrayPizza = [
+  {
+    name: "PROFESSEUR",
+    picture: "./src/Professeur-perso.jpg"
+  },
+  {
+    name: "TOKYO",
+    picture: "./src/Tokyo-perso.jpg"
+  },
+  {
+    name: "LISBONNE",
+    picture: "./src/Lisbonne-perso.jpg"
+  },
+  {
+    name: "BERLIN",
+    picture: "./src/Berlin-perso.jpg"
+  },
+  {
+    name: "RIO",
+    picture: "./src/Rio-perso.jpg"
+  },
+  {
+    name: "NAIROBI",
+    picture: "./src/Nairobi-perso.jpg"
+  },
+  {
+    name: "DENVER",
+    picture: "./src/Denver-perso.jpg"
+  },
+  {
+    name: "MOSCOU",
+    picture: "./src/Moscou-perso.jpg"
+  },
+  {
+    name: "STOCKHOLM",
+    picture: "./src/Stockholm-perso.jpg"
+  },
+  {
+    name: "PALERME",
+    picture: "./src/Palerme-perso.jpg"
+  },
+  {
+    name: "BOGOTA",
+    picture: "./src/Bogota-perso.jpg"
+  }
+]
+
+button.addEventListener('click', function(){
+  setInterval(
+    function() {
+      let randomNbr = Math.floor(Math.random() *12);
+      const pizza = arrayPizza[randomNbr];
+      professeur.src = pizza.picture;
+      console.log(pizza);
+      prenom.innerHTML = pizza.name;
+    
+    },
+    200
+  );
+})
+
+
+
+/*function stopTextColor() {
+  clearInterval(randomNbr);
+  // release our intervalID from the variable
+  randomNbr = null; 
+}*/
+
+
+
